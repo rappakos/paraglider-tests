438bd404db63b3489c2129d9ce1250319cf882b0
rappakos <rappakos@gmail.com>
2023-11-05 09:11:15 +0100

    glider tests - button to refresh test list

--- glider_tests_app/routes.py
+++ glider_tests_app/routes.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from .views import index,testdata
+from .views import index,testdata,load_tests
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 
@@ -8,3 +8,4 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 def setup_routes(app):
     app.router.add_get('/', index)
     app.router.add_get('/{org}', testdata, name='testdata')
+    app.router.add_post('/{org}', load_tests, name='load_tests')
--- glider_tests_app/views.py
+++ glider_tests_app/views.py
@@ -10,8 +10,8 @@ ORGS = {
     'dhv': {'name': 'DHV'}
 }
 
-def redirect(router, route_name):
-    location = router[route_name].url_for()
+def redirect(router, route_name, org = None):
+    location = router[route_name].url_for(org=org)
     return web.HTTPFound(location)
 
 @aiohttp_jinja2.template('index.html')
@@ -26,7 +26,17 @@ async def testdata(request):
     org = request.match_info.get('org', None)
     if org in ORGS:
         return {
-            'org': ORGS[org]            
+            'org': org,
+            'orgdata': ORGS[org]            
         }
     else:
          raise web.HTTPNotFound(reason="Organization not available")
+    
+async def load_tests(request):
+    org = request.match_info.get('org', None)    
+    if request.method == 'POST':
+        print("TODO", org)
+
+        raise redirect(request.app.router, 'testdata', org=org)
+    else:
+        raise NotImplementedError("invalid?")        
